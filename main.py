f64f22071ff17d77275e8fdda02cbf8d4dbef2d8
tridibbanik17 <165870312+tridibbanik17@users.noreply.github.com>
2024-12-25 18:39:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,12 @@
 
 # This Python program controls a Q-Arm to sort various containers differentiated by their masses, build materials and container's destination. 
 
+# The Q-arm only accepts the containers of one category in a signle cycle.
+
+# The Q-bot's hopper can carry a maximum of 3 containers.
+
+# If the max number of containers is reached, or a different type of container is served in the table, the Q-Arm stops and Q-bot starts moving.
+
 # The program also controls the Q-bot to follow a specific yellow line detected by IR sensors mounted on the Q-bot 
 # and dump the previously picked containers into one of four recycling bins differentiated by the bin colours (red, green, blue, and white). 
 
@@ -9,7 +15,9 @@
 
 # After dumping is completed, the Q-bot returns to the home position and a new cycle of sorting and recycling begins.
 
-# An infinite number of cycles will run unless the user terminates the program. Quanser powers the simulated virtual environment.
+# An infinite number of cycles will run unless the user terminates the program. 
+
+# Quanser Interactive Labs software powers the simulated virtual environment.
 
 ip_address = 'localhost' # Enter your IP Address here
 project_identifier = 'P3B' # Enter the project identifier i.e. P2A or P2B
